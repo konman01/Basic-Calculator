3adcf299df824ced066bfac578fdcc005e225315
konman01 <konimanjunatha@gmail.com>
2021-03-26 01:23:18 -0700

    Added indirect function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,28 +1,37 @@
 const defaultVal = 0;
 
-let currentResult;
+let currentResult = defaultVal;
+//let result;
 
 // function to add number
 // it is not important where we define function
-function add(num1, num2) {
+function add() {
     // defaultVal is at the global scope
     // result is at the local scope to the function
-    const result = num1 + num2;
-    return result;
+    // function can access the global variable
+    // local variable cannot be used outside the function
+    //result = num1 + num2;
+
+    let returnResult = currentResult + userInput.value;
+    let calculationDesc = ` (${defaultVal} + 10) * 3 / 2 - 1 `; // reduces the concatination
+    outputResult(returnResult, calculationDesc);
+    // any statement after return does not execute
+
 }
 
+// add function will only be executed when the button is clicked
+addBtn.addEventListener('click', add);
 
-currentResult = add(1, 2);
-    
-    
+//currentResult = add(1, 2);
+
 //let calculationDesc = '(0 + 10) * 3 / 2 - 1';
 
 //let calculationDesc = '(' + currentResult + ' + 10) * 3 / 2 - 1';
 
-let calculationDesc = ` (${defaultVal} + 10) * 3 / 2 - 1 `; // reduces the concatination
+
 
 let errorMessage = ' error message is here \''; // \ is the escape character
 
 
-outputResult(currentResult, calculationDesc);
+
 
